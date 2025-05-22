8ec856d298151f8570d2648c0f6b5d8f671baf1b
Zheng-Changfu <zcfpromise@163.com>
2025-05-22 15:33:25 +0800

    fix: 修复 keep 方法在遍历键时未检查对象属性存在性的问题

--- packages/components/src/_utils/keep.ts
+++ packages/components/src/_utils/keep.ts
@@ -4,8 +4,10 @@ export function keep<T, K, R>(
   rest?: R,
 ): Pick<T, K & keyof T> & R {
   const keepedObject: any = {}
-  keys.forEach((key) => {
-    keepedObject[key] = (object as any)[key]
+  keys.forEach((key: any) => {
+    if (key in (object as any)) {
+      keepedObject[key] = (object as any)[key]
+    }
   })
   return Object.assign(keepedObject, rest)
 }
