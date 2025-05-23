39e77cd13c181fd5355144ebc379b6487f8ed7de
Zheng-Changfu <zcfpromise@163.com>
2025-05-24 00:37:58 +0800

    pref: 优化代码可读性

--- packages/components/src/form/components/cascader/components/cascader.tsx
+++ packages/components/src/form/components/cascader/components/cascader.tsx
@@ -8,6 +8,8 @@ import { computed, defineComponent } from 'vue'
 import { useFieldUtils } from '../../field'
 import { useInjectCascaderInstStore } from '../inst'
 
+type ValueAtom = string | number
+
 export default defineComponent({
   name: 'Cascader',
   inheritAttrs: false,
@@ -34,91 +36,70 @@ export default defineComponent({
       }
     })
 
-    const selectedLabels = computed(() => {
+    const optionValueToInfoMap = computed(() => {
       const {
-        renderLabel,
         options = [],
-        labelField = 'label',
         valueField = 'value',
         childrenField = 'children',
-        showPath = true,
-        separator = ' / ',
-      } = props as any
+      } = props
 
+      const map = new Map<ValueAtom, {
+        option: CascaderOption
+        parentValue: ValueAtom | null
+      }>()
+
+      eachTree(
+        options as CascaderOption[],
+        (item, _, { parent }) => {
+          const value = get(item, valueField) as ValueAtom
+          map.set(value, {
+            option: item,
+            parentValue: get(parent, valueField, null) as ValueAtom | null,
+          })
+        },
+        childrenField,
+      )
+      return map
+    })
+
+    const selectedLabels = computed(() => {
       const labels: VNodeChild[] = []
       const selectedValue = isArray(value.value) ? value.value : [value.value]
-
-      if (showPath) {
-        // 查找完整路径
-        const findPathForValue = (value: any) => {
-          const path: VNodeChild[] = []
-
-          const findPath = (options: CascaderOption[], parentPath: VNodeChild[] = []) => {
-            for (const option of options) {
-              const optionValue = get(option, valueField)
-              let optionLabel = get(option, labelField) as VNodeChild
-              if (renderLabel) {
-                optionLabel = renderLabel(option as any, true)
-              }
-
-              const currentPath = [...parentPath, optionLabel]
-
-              if (optionValue === value) {
-                path.push(...currentPath)
-                return true
-              }
-
-              const children = get(option, childrenField) as CascaderOption[] | undefined
-              if (children && children.length > 0) {
-                if (findPath(children, currentPath)) {
-                  return true
-                }
-              }
-            }
-            return false
-          }
-
-          findPath(options as CascaderOption[])
-          return path
-        }
-
-        // 为每个选中的值找到路径并添加到labels中
-        for (const val of selectedValue) {
-          const path = findPathForValue(val)
-          if (path.length > 0) {
-            labels.push(
-              <span>
-                {path.map((item, index) => [
-                  index > 0 ? separator : null,
-                  <span>{item}</span>,
-                ])}
-              </span>,
-            )
-          }
-        }
+      for (const value of selectedValue) {
+        const labelNodes = findLabelNodesByValue(value as ValueAtom)
+        labels.push(...labelNodes)
       }
-      else {
-        // 原来的逻辑：仅显示选中节点的标签
-        eachTree(
-          options as CascaderOption[],
-          (item) => {
-            const value = get(item, valueField)
-            if (selectedValue.includes(value)) {
-              let label = get(item, labelField) as VNodeChild
-              if (renderLabel) {
-                label = renderLabel(item as any, true)
-              }
-              if (label) {
-                labels.push(<span>{label}</span>)
-              }
-            }
-          },
-          childrenField,
-        )
-      }
-
       return labels
     })
+
+    function findLabelNodesByValue(value: ValueAtom) {
+      const {
+        renderLabel,
+        showPath = true,
+        separator = ' / ',
+        labelField = 'label',
+      } = props
+
+      const labels: VNodeChild[] = []
+      let info = optionValueToInfoMap.value.get(value)
+      while (info) {
+        let label = get(info.option, labelField) as VNodeChild
+        if (renderLabel) {
+          label = renderLabel(info.option, true)
+        }
+        if (label) {
+          if (labels.length) {
+            labels.unshift(separator)
+          }
+          labels.unshift(<span>{label}</span>)
+        }
+        if (!showPath) {
+          break
+        };
+        info = optionValueToInfoMap.value.get(info.parentValue as any)
+      }
+      return labels
+    }
 
     registerInst({
       blur: () => instRef.value?.blur(),
--- packages/components/src/form/components/tree-select/components/tree-select.tsx
+++ packages/components/src/form/components/tree-select/components/tree-select.tsx
@@ -8,6 +8,8 @@ import { computed, defineComponent, ref } from 'vue'
 import { useFieldUtils } from '../../field'
 import { useInjectTreeSelectInstStore } from '../inst'
 
+type Key = string | number
+
 export default defineComponent({
   name: 'TreeSelect',
   // 这个 props 类型复杂会导致构建类型声明文件失败，先用 any 解决
@@ -28,93 +30,81 @@ export default defineComponent({
       emptyDom,
     } = useFieldUtils()
 
-    const selectedLabels = computed(() => {
-      const {
-        renderTag,
-        renderLabel,
-        options = [],
-        keyField = 'key',
-        labelField = 'label',
-        childrenField = 'children',
-        showPath = false,
-        separator = ' / ',
-      } = props as any
-
-      const labels: VNodeChild[] = []
-      const selectedValue = isArray(value.value) ? value.value : [value.value]
-
-      if (showPath) {
-        // 查找完整路径
-        const findPathForValue = (val: any) => {
-          const path: VNodeChild[] = []
-          const findPath = (opts: TreeSelectOption[], parentPath: VNodeChild[] = []) => {
-            for (const option of opts) {
-              const optionValue = get(option, keyField)
-              let optionLabel = get(option, labelField) as VNodeChild
-              if (renderTag) {
-                optionLabel = renderTag({ option: option as any, handleClose: noop })
-              }
-              if (renderLabel) {
-                optionLabel = renderLabel({ option, checked: true, selected: true })
-              }
-              const currentPath = [...parentPath, optionLabel]
-              if (optionValue === val) {
-                path.push(...currentPath)
-                return true
-              }
-              const children = get(option, childrenField) as TreeSelectOption[] | undefined
-              if (children && children.length > 0) {
-                if (findPath(children, currentPath)) {
-                  return true
-                }
-              }
-            }
-            return false
-          }
-          findPath(options as TreeSelectOption[])
-          return path
-        }
-        for (const val of selectedValue) {
-          const path = findPathForValue(val)
-          if (path.length > 0) {
-            labels.push(
-              <span>
-                {path.map((item, index) => [index > 0 ? separator : null, <span>{item}</span>])}
-              </span>,
-            )
-          }
-        }
-      }
-      else {
-        eachTree(
-          options as TreeSelectOption[],
-          (item) => {
-            const value = get(item, keyField)
-            if (selectedValue.includes(value)) {
-              let label = get(item, labelField) as VNodeChild
-              if (renderTag) {
-                label = renderTag({ option: item as any, handleClose: noop })
-              }
-              if (renderLabel) {
-                label = renderLabel({ option: item, checked: true, selected: true })
-              }
-              if (label) {
-                labels.push(<span>{label}</span>)
-              }
-            }
-          },
-          childrenField,
-        )
-      }
-      return labels
-    })
-
     const nTreeSelectProps = computed(() => {
       return {
         ...props,
         value: props.value ?? null,
       }
     })
+
+    const optionKeyToInfoMap = computed(() => {
+      const {
+        options = [],
+        keyField = 'key',
+        childrenField = 'children',
+      } = props
+
+      const map = new Map<Key, {
+        option: TreeSelectOption
+        parentValue: Key | null
+      }>()
+
+      eachTree(
+        options as TreeSelectOption[],
+        (item, _, { parent }) => {
+          const key = get(item, keyField) as Key
+          map.set(key, {
+            option: item,
+            parentValue: get(parent, keyField, null) as Key | null,
+          })
+        },
+        childrenField,
+      )
+      return map
+    })
+
+    const selectedLabels = computed(() => {
+      const labels: VNodeChild[] = []
+      const selectedValue = isArray(value.value) ? value.value : [value.value]
+      for (const value of selectedValue) {
+        const labelNodes = findLabelNodesByKey(value as Key)
+        labels.push(...labelNodes)
+      }
+      return labels
+    })
+
+    function findLabelNodesByKey(key: Key) {
+      const {
+        renderTag,
+        renderLabel,
+        showPath = false,
+        separator = ' / ',
+        labelField = 'label',
+      } = props
+
+      const labels: VNodeChild[] = []
+      let info = optionKeyToInfoMap.value.get(key)
+      while (info) {
+        let label = get(info.option, labelField) as VNodeChild
+        if (renderTag) {
+          label = renderTag({ option: info.option, handleClose: noop })
+        }
+        if (renderLabel) {
+          label = renderLabel({ option: info.option, checked: true, selected: true })
+        }
+        if (label) {
+          if (labels.length) {
+            labels.unshift(separator)
+          }
+          labels.unshift(<span>{label}</span>)
+        }
+        if (!showPath) {
+          break
+        };
+        info = optionKeyToInfoMap.value.get(info.parentValue as any)
+      }
+      return labels
+    }
 
     registerInst({
       blur: () => instRef.value?.blur(),
