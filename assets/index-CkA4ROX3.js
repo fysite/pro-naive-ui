1e4c1d89a182da59949136b18e5d95a5d174359d
fysite <fysite@users.noreply.github.com>
2025-06-10 08:40:54 +0000

    deploy: a1ad975285f56c5f00a298e8a71be290b4146124

---
+++ assets/index-CkA4ROX3.js
@@ -0,0 +1,111 @@
+import{Y as t,J as y,K as a,a0 as N,R as l,$ as n,O as v,c as k,L as w,j as s}from"./vue-yiQq_PH0.js";import{_ as P,u as A,a as M}from"./index-BctytEVQ.js";import{at as F}from"./naive-ui-CAwxRKuH.js";import"./lodash-es-B1kh5og4.js";import"./vue-router-IiN1iUNi.js";import"./highlight.js-Dis82wzk.js";import"./vicons-ySDBKu-B.js";const T={components:{},setup(){const i=A(),h=F(()=>!i.value),f=i;return{showAnchor:h,displayMode:M(),wrapperStyle:k(()=>f.value?"padding: 16px 16px 24px 16px;":"display: flex; flex-wrap: nowrap; padding: 32px 24px 56px 56px;"),contentStyle:k(()=>h.value?"width: calc(100% - 228px); margin-right: 36px;":"width: 100%; padding-right: 12px;"),url:"src/pages/docs/installation/zhCN/index.md"}}},U=a("pre",null,[a("span",{class:"hljs-meta"},[s("<!DOCTYPE "),a("span",{class:"hljs-keyword"},"html"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"html"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"head"),s(">")]),s(`
+    `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"meta"),s(),a("span",{class:"hljs-attr"},"charset"),s("="),a("span",{class:"hljs-string"},'"utf-8"'),s(" />")]),s(`
+    `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"script"),s(),a("span",{class:"hljs-attr"},"src"),s("="),a("span",{class:"hljs-string"},'"https://unpkg.com/vue"'),s(">")]),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"script"),s(">")]),s(`
+    `),a("span",{class:"hljs-comment"},"<!-- 会使用最新版本，你最好指定一个版本 -->"),s(`
+    `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"script"),s(),a("span",{class:"hljs-attr"},"src"),s("="),a("span",{class:"hljs-string"},'"https://unpkg.com/naive-ui"'),s(">")]),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"script"),s(">")]),s(`
+    `),a("span",{class:"hljs-comment"},"<!-- 会使用最新版本，你最好指定一个版本 -->"),s(`
+    `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"script"),s(),a("span",{class:"hljs-attr"},"src"),s("="),a("span",{class:"hljs-string"},'"https://unpkg.com/pro-naive-ui"'),s(">")]),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"script"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"head"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"body"),s(">")]),s(`
+    `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"div"),s(),a("span",{class:"hljs-attr"},"id"),s("="),a("span",{class:"hljs-string"},'"app"'),s(">")]),s(`
+      `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"pro-card"),s(),a("span",{class:"hljs-attr"},"title"),s("="),a("span",{class:"hljs-string"},'"title"'),s(">")]),s(`
+        {{ message }}
+      `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s(`
+    `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"div"),s(">")]),s(`
+    `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"script"),s(">")]),a("span",{class:"language-javascript"},[s(`
+      `),a("span",{class:"hljs-keyword"},"const"),s(),a("span",{class:"hljs-title class_"},"App"),s(` = {
+        `),a("span",{class:"hljs-title function_"},"setup"),s("("),a("span",{class:"hljs-params"}),s(`) {
+          `),a("span",{class:"hljs-keyword"},"return"),s(` {
+            `),a("span",{class:"hljs-attr"},"message"),s(": "),a("span",{class:"hljs-string"},"'pro-naive'"),s(`
+          }
+        }
+      }
+      `),a("span",{class:"hljs-keyword"},"const"),s(" app = "),a("span",{class:"hljs-title class_"},"Vue"),s("."),a("span",{class:"hljs-title function_"},"createApp"),s("("),a("span",{class:"hljs-title class_"},"App"),s(`)
+      app.`),a("span",{class:"hljs-title function_"},"use"),s(`(proNaive)
+      app.`),a("span",{class:"hljs-title function_"},"mount"),s("("),a("span",{class:"hljs-string"},"'#app'"),s(`)
+    `)]),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"script"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"body"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"html"),s(">")])],-1),D=a("pre",null,[a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s("pro-naive-ui"),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+
+`),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"script"),s(">")]),a("span",{class:"language-javascript"},[s(`
+  `),a("span",{class:"hljs-keyword"},"import"),s(" { defineComponent } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'vue'"),s(`
+  `),a("span",{class:"hljs-keyword"},"import"),s(" { "),a("span",{class:"hljs-title class_"},"ProCard"),s(" } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'pro-naive-ui'"),s(`
+
+  `),a("span",{class:"hljs-keyword"},"export"),s(),a("span",{class:"hljs-keyword"},"default"),s(),a("span",{class:"hljs-title function_"},"defineComponent"),s(`({
+    `),a("span",{class:"hljs-attr"},"components"),s(`: {
+      `),a("span",{class:"hljs-title class_"},"ProCard"),s(`
+    }
+  })
+`)]),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"script"),s(">")])],-1),R=a("pre",null,[a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s("pro-naive-ui"),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+
+`),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"script"),s(),a("span",{class:"hljs-attr"},"setup"),s(">")]),a("span",{class:"language-javascript"},[s(`
+  `),a("span",{class:"hljs-keyword"},"import"),s(" { "),a("span",{class:"hljs-title class_"},"ProCard"),s(" } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'pro-naive-ui'"),s(`
+`)]),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"script"),s(">")])],-1),V=a("pre",null,[a("span",{class:"hljs-keyword"},"import"),s(" proNaive "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'pro-naive-ui'"),s(`
+`),a("span",{class:"hljs-keyword"},"import"),s(" { createApp } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'vue'"),s(`
+
+`),a("span",{class:"hljs-keyword"},"const"),s(" app = "),a("span",{class:"hljs-title function_"},"createApp"),s("("),a("span",{class:"hljs-title class_"},"App"),s(`)
+app.`),a("span",{class:"hljs-title function_"},"use"),s("(proNaive)")],-1),z=a("pre",null,[a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s("pro-naive-ui"),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")])],-1),I=a("pre",null,[a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"script"),s(">")]),a("span",{class:"language-javascript"},[s(`
+  `),a("span",{class:"hljs-keyword"},"import"),s(" { defineComponent } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'vue'"),s(`
+  `),a("span",{class:"hljs-keyword"},"import"),s(" { "),a("span",{class:"hljs-title class_"},"ProConfigProvider"),s(", "),a("span",{class:"hljs-title class_"},"ProCard"),s(" } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'pro-naive-ui'"),s(`
+  `),a("span",{class:"hljs-comment"},"// locale"),s(`
+  `),a("span",{class:"hljs-keyword"},"import"),s(" { zhCN } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'pro-naive-ui'"),s(`
+
+  `),a("span",{class:"hljs-keyword"},"export"),s(),a("span",{class:"hljs-keyword"},"default"),s(),a("span",{class:"hljs-title function_"},"defineComponent"),s(`({
+    `),a("span",{class:"hljs-attr"},"components"),s(`: {
+      `),a("span",{class:"hljs-title class_"},"ProConfigProvider"),s(`,
+      `),a("span",{class:"hljs-title class_"},"ProCard"),s(`,
+    },
+    `),a("span",{class:"hljs-title function_"},"setup"),s("("),a("span",{class:"hljs-params"}),s(`) {
+      `),a("span",{class:"hljs-keyword"},"return"),s(` {
+        zhCN,
+      }
+    }
+  })
+`)]),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"script"),s(">")]),s(`
+
+`),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"pro-config-provider"),s(),a("span",{class:"hljs-attr"},":locale"),s("="),a("span",{class:"hljs-string"},'"zhCN"'),s(">")]),s(`
+    `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s("pro-naive-ui"),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"pro-config-provider"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+
+`),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"style"),s(">")]),a("span",{class:"language-css"},[s(`
+  `),a("span",{class:"hljs-selector-tag"},"body"),s(` {
+    `),a("span",{class:"hljs-attribute"},"background"),s(`: black;
+  }
+`)]),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"style"),s(">")])],-1),B=a("pre",null,[a("span",{class:"hljs-keyword"},"import"),s(" vue "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'@vitejs/plugin-vue'"),s(`
+`),a("span",{class:"hljs-keyword"},"import"),s(" { "),a("span",{class:"hljs-title class_"},"ProNaiveUIResolver"),s(" } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'pro-naive-ui-resolver'"),s(`
+`),a("span",{class:"hljs-keyword"},"import"),s(),a("span",{class:"hljs-title class_"},"Components"),s(),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'unplugin-vue-components/vite'"),s(`
+`),a("span",{class:"hljs-comment"},"// vite.config.ts"),s(`
+`),a("span",{class:"hljs-keyword"},"import"),s(" { defineConfig } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'vite'"),s(`
+
+`),a("span",{class:"hljs-comment"},"// https://vitejs.dev/config/"),s(`
+`),a("span",{class:"hljs-keyword"},"export"),s(),a("span",{class:"hljs-keyword"},"default"),s(),a("span",{class:"hljs-title function_"},"defineConfig"),s(`({
+  `),a("span",{class:"hljs-attr"},"plugins"),s(`: [
+    `),a("span",{class:"hljs-title function_"},"vue"),s(`(),
+    `),a("span",{class:"hljs-title class_"},"Components"),s(`({
+      `),a("span",{class:"hljs-attr"},"resolvers"),s(": ["),a("span",{class:"hljs-title class_"},"ProNaiveUIResolver"),s(`()]
+    })
+  ]
+})`)],-1),q=a("pre",null,[a("span",{class:"hljs-keyword"},"import"),s(` {
+  create,
+  `),a("span",{class:"hljs-title class_"},"ProCard"),s(`,
+} `),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'pro-naive-ui'"),s(`
+`),a("span",{class:"hljs-keyword"},"import"),s(" { createApp } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'vue'"),s(`
+
+`),a("span",{class:"hljs-keyword"},"const"),s(" proNaive = "),a("span",{class:"hljs-title function_"},"create"),s(`({
+  `),a("span",{class:"hljs-attr"},"components"),s(": ["),a("span",{class:"hljs-title class_"},"ProCard"),s(`]
+})
+
+`),a("span",{class:"hljs-keyword"},"const"),s(" app = "),a("span",{class:"hljs-title function_"},"createApp"),s(`()
+app.`),a("span",{class:"hljs-title function_"},"use"),s("(proNaive)")],-1),E=a("pre",null,[a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s("pro-naive-ui"),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"pro-card"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")])],-1),O={key:0,style:{width:"192px"}};function Y(i,h,f,j,$,J){const b=t("n-h1"),e=t("n-p"),x=t("n-blockquote"),p=t("n-code"),c=t("n-card"),u=t("n-tab-pane"),C=t("n-tabs"),m=t("n-h2"),r=t("n-text"),o=t("n-scrollbar"),d=t("n-h3"),g=t("n-a"),_=t("n-anchor-link"),S=t("n-anchor");return w(),y("div",{class:"doc",style:v(j.wrapperStyle)},[a("div",{style:v(j.contentStyle)},[l(b,{id:"安装"},{default:n(()=>[s("安装")]),_:1}),l(x,null,{default:n(()=>[l(e,null,{default:n(()=>[s("注意，pro-naive-ui 依赖 naive-ui。")]),_:1})]),_:1}),l(C,{type:"line",animated:""},{default:n(()=>[l(u,{name:"npm",tab:"npm"},{default:n(()=>[l(c,{embedded:""},{default:n(()=>[l(p,null,{default:n(()=>[s("npm i pro-naive-ui")]),_:1})]),_:1})]),_:1}),l(u,{name:"pnpm",tab:"pnpm"},{default:n(()=>[l(c,{embedded:""},{default:n(()=>[l(p,null,{default:n(()=>[s("pnpm i pro-naive-ui")]),_:1})]),_:1})]),_:1}),l(u,{name:"yarn",tab:"yarn"},{default:n(()=>[l(c,{embedded:""},{default:n(()=>[l(p,null,{default:n(()=>[s("yarn add pro-naive-ui")]),_:1})]),_:1})]),_:1})]),_:1}),l(m,{id:"UMD"},{default:n(()=>[s("UMD")]),_:1}),l(e,null,{default:n(()=>[s("如果你要使用 minify 版本的包，将 "),l(r,{code:""},{default:n(()=>[s("https://unpkg.com/pro-naive-ui@version/dist/index.prod.js")]),_:1}),s(" 作为 "),l(r,{code:""},{default:n(()=>[s("src")]),_:1}),s("，"),l(r,{code:""},{default:n(()=>[s("version")]),_:1}),s(" 是你期望使用的版本，如果不指定 "),l(r,{code:""},{default:n(()=>[s("version")]),_:1}),s(" 则会使用最新的版本。")]),_:1}),l(e,null,{default:n(()=>[s("你最好锁定包的版本，不然可能会有不兼容变更。")]),_:1}),l(e,null,{default:n(()=>[s("下面是个基本 demo")]),_:1}),l(c,{embedded:"",bordered:!1,class:"md-card","content-style":"padding: 0;"},{default:n(()=>[l(o,{"x-scrollable":"","content-style":"padding: 16px;"},{default:n(()=>[l(p,null,{default:n(()=>[U]),_:1})]),_:1})]),_:1}),l(m,{id:"在-SFC-中使用"},{default:n(()=>[s("在 SFC 中使用")]),_:1}),l(e,null,{default:n(()=>[s("如果你想使用单文件组件风格（SFC - Single File Component），可以选择直接引入或全局安装在 Vue App 中。")]),_:1}),l(d,{id:"直接引入（推荐）"},{default:n(()=>[s("直接引入（推荐）")]),_:1}),l(e,null,{default:n(()=>[s("你可以直接导入组件并使用它。这种情况下，只有导入的组件才会被打包。")]),_:1}),l(e,null,{default:n(()=>[s("如果你想知道如何按需引入主题和语言包，请参考 "),l(g,{href:"#按需引入（Tree-Shaking）"},{default:n(()=>[s("按需引入")]),_:1}),s("。")]),_:1}),l(c,{embedded:"",bordered:!1,class:"md-card","content-style":"padding: 0;"},{default:n(()=>[l(o,{"x-scrollable":"","content-style":"padding: 16px;"},{default:n(()=>[l(p,null,{default:n(()=>[D]),_:1})]),_:1})]),_:1}),l(e,null,{default:n(()=>[s("如果你可以使用 setup script，你可以用下面的方式使用组件。")]),_:1}),l(c,{embedded:"",bordered:!1,class:"md-card","content-style":"padding: 0;"},{default:n(()=>[l(o,{"x-scrollable":"","content-style":"padding: 16px;"},{default:n(()=>[l(p,null,{default:n(()=>[R]),_:1})]),_:1})]),_:1}),l(d,{id:"全局安装（不推荐）"},{default:n(()=>[s("全局安装（不推荐）")]),_:1}),l(e,null,{default:n(()=>[s("失去 tree-shaking 的能力，打包有冗余代码。")]),_:1}),l(e,null,{default:n(()=>[s("如果你想全局安装但是不想安装全部组件，请参考 "),l(g,{href:"#按需引入（Tree-Shaking）"},{default:n(()=>[s("按需引入")]),_:1}),s("。")]),_:1}),l(c,{embedded:"",bordered:!1,class:"md-card","content-style":"padding: 0;"},{default:n(()=>[l(o,{"x-scrollable":"","content-style":"padding: 16px;"},{default:n(()=>[l(p,null,{default:n(()=>[V]),_:1})]),_:1})]),_:1}),l(e,null,{default:n(()=>[s("安装后，你可以这样在 SFC 中使用全部组件。")]),_:1}),l(c,{embedded:"",bordered:!1,class:"md-card","content-style":"padding: 0;"},{default:n(()=>[l(o,{"x-scrollable":"","content-style":"padding: 16px;"},{default:n(()=>[l(p,null,{default:n(()=>[z]),_:1})]),_:1})]),_:1}),l(m,{id:"按需引入（Tree-Shaking）"},{default:n(()=>[s("按需引入（Tree Shaking）")]),_:1}),l(e,null,{default:n(()=>[s("Pro Naive UI 支持 tree shaking，组件、语言均可 tree-shaking。")]),_:1}),l(e,null,{default:n(()=>[s("默认情况语言为英文，无需额外导入。")]),_:1}),l(d,{id:"手动引入"},{default:n(()=>[s("手动引入")]),_:1}),l(c,{embedded:"",bordered:!1,class:"md-card","content-style":"padding: 0;"},{default:n(()=>[l(o,{"x-scrollable":"","content-style":"padding: 16px;"},{default:n(()=>[l(p,null,{default:n(()=>[I]),_:1})]),_:1})]),_:1}),l(d,{id:"自动引入"},{default:n(()=>[s("自动引入")]),_:1}),l(e,null,{default:n(()=>[s("可以使用 "),l(r,{code:""},{default:n(()=>[s("unplugin-vue-components")]),_:1}),s(" 插件和 "),l(r,{code:""},{default:n(()=>[s("pro-naive-ui-resolver")]),_:1}),s(" 来自动导入组件。")]),_:1}),l(c,{embedded:"",bordered:!1,class:"md-card","content-style":"padding: 0;"},{default:n(()=>[l(o,{"x-scrollable":"","content-style":"padding: 16px;"},{default:n(()=>[l(p,null,{default:n(()=>[B]),_:1})]),_:1})]),_:1}),l(d,{id:"按需全局安装组件（手动）"},{default:n(()=>[s("按需全局安装组件（手动）")]),_:1}),l(c,{embedded:"",bordered:!1,class:"md-card","content-style":"padding: 0;"},{default:n(()=>[l(o,{"x-scrollable":"","content-style":"padding: 16px;"},{default:n(()=>[l(p,null,{default:n(()=>[q]),_:1})]),_:1})]),_:1}),l(e,null,{default:n(()=>[s("安装后，你可以这样在 SFC 中使用你安装的组件。")]),_:1}),l(c,{embedded:"",bordered:!1,class:"md-card","content-style":"padding: 0;"},{default:n(()=>[l(o,{"x-scrollable":"","content-style":"padding: 16px;"},{default:n(()=>[l(p,null,{default:n(()=>[E]),_:1})]),_:1})]),_:1})],4),j.showAnchor?(w(),y("div",O,[l(S,{"internal-scrollable":"",bound:16,type:"block",style:{width:"192px",position:"sticky",top:"32px","max-height":"calc(100vh - 32px - 64px)",height:"auto"},"offset-target":"#doc-layout","ignore-gap":!0},{default:n(()=>[l(_,{title:"UMD",href:"#UMD"}),l(_,{title:"在 SFC 中使用",href:"#在-SFC-中使用"}),l(_,{title:"按需引入（Tree Shaking）",href:"#按需引入（Tree-Shaking）"})]),_:1})])):N("",!0)],4)}const Z=P(T,[["render",Y]]);export{Z as default};
