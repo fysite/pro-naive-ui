c8a12acd7400300f689cd90f416a333c6de490bb
fysite <fysite@users.noreply.github.com>
2025-06-10 08:17:55 +0000

    deploy: 040d5d90cd3ff753823ca316911138019028a60c

---
+++ assets/a-QPxvhlve.js
@@ -0,0 +1,8 @@
+import{k as c,m as d,c as m}from"./vue-yiQq_PH0.js";import{aj as u,aw as h,ax as r,aA as f}from"./index-B0ZkSKYK.js";import{t as p}from"./p-DxcKq6JT.js";const v=u("a",`
+ cursor: pointer;
+ transition:
+ color .3s var(--n-bezier),
+ text-decoration-color .3s var(--n-bezier);
+ text-decoration-color: var(--n-text-color);
+ color: var(--n-text-color);
+`),x=Object.assign({},r.props),y=c({name:"A",props:x,setup(e){const{mergedClsPrefixRef:o,inlineThemeDisabled:t}=h(e),a=r("Typography","-a",v,p,e,o),n=m(()=>{const{common:{cubicBezierEaseInOut:i},self:{aTextColor:l}}=a.value;return{"--n-text-color":l,"--n-bezier":i}}),s=t?f("a",void 0,n,e):void 0;return{mergedClsPrefix:o,cssVars:t?void 0:n,themeClass:s==null?void 0:s.themeClass,onRender:s==null?void 0:s.onRender}},render(){var e;return(e=this.onRender)===null||e===void 0||e.call(this),d("a",{class:[`${this.mergedClsPrefix}-a`,this.themeClass],style:this.cssVars},this.$slots)}});export{y as _};
